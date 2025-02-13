c09e4534006e4485212f6e78c9e28dc51b9979b6
Automan1218 <2984335643@qq.com>
2025-02-13 14:29:08 +0800

    update test

--- src/test/java/dev/team08/movie_verse_backend/MovieVerseBackendApplicationTests.java
+++ src/test/java/dev/team08/movie_verse_backend/MovieVerseBackendApplicationTests.java
@@ -2,14 +2,11 @@ package dev.team08.movie_verse_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(
-        properties = {
-                "spring.datasource.url=jdbc:mysql://mysql:3306/movie_verse_pivot",
-                "spring.datasource.username=user",
-                "spring.datasource.password=123456"
-        }
-)
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;;
+@ActiveProfiles("test")
+@SpringBootTest
+@Import(TestDataSourceConfig.class)
 class MovieVerseBackendApplicationTests {
 
     @Test
